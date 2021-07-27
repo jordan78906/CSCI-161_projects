c986aba65d90077cfea3968418f0736601219319
jordan78906 <64317130+jordan78906@users.noreply.github.com>
2021-07-27 00:40:52 -0500

    Test
    Code Test Environment

---
+++ HernandezAlmache_Jordan_1.py
@@ -0,0 +1,26 @@
+
+#Jordan Hernandez-Alamche
+#CSci 161 L03
+#Assignment 1
+
+print("Hello World!")
+
+#Code Test enviornment
+'''
+list_a = [{'fox':[10,1.00,'hello']},
+          {'pig':[20,3.00,'trouble']},
+          {'rat':[30,5.00,'myth']},
+          {'dog':[40,10.00,'free']},
+          {'cat':[50,2.00,'light']}
+         ]
+customer_name = 'John Doe'
+date = 'Feb 15'
+num_items = 12
+item_cost = 32.00
+item_description = 'abcdefg'
+total_price = num_items * item_cost
+'''
+
+
+
+
