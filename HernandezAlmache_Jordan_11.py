2d277e323d6dcc6f894324ff4a2aead4f3a61d26
jordan78906 <64317130+jordan78906@users.noreply.github.com>
2021-07-27 01:58:13 -0500

    Merge Sort Algorithm

---
+++ HernandezAlmache_Jordan_11.py
@@ -0,0 +1,74 @@
+
+#Jordan Hernandez-Alamche
+#CSci 161 L03
+#Assignment 11
+#Merge sort
+'''
+-Merge Sort:
+1:Find Mid point to divide the array into two halves
+2:Call Merge Sort for first half
+3:Call Merge Sort for second half
+4:Base Case: if array size is 1 or smaller, return.
+5:Merge the divided arrays sorted until the complete array is merged.
+'''
+def mergeSort(main_list):
+
+    #1:Mid-Point - Slicing array in half
+    #Left array from 0-mid    Right array from mid-end
+    L_list = main_list[:len(main_list)//2]
+    R_list = main_list[len(main_list)//2:]
+
+    #4: Base Case - Already sorted, since its a single element.
+    if len(main_list) <= 1:
+        return main_list
+
+    #2&3: Recursion - slices left & right arrays to the smaller arrays, length of 1 element  
+    mergeSort(L_list)
+    mergeSort(R_list)
+    
+    
+    #5: Merge - Merge divided arrays sorted 
+    merge_sorted_arrays(L_list,R_list,main_list)
+    
+
+def merge_sorted_arrays(L_list,R_list,main_list):
+    #Merge
+ 
+    #keeping track of index positions
+    #i = left index, j = right index, k = merged index
+    i = j = k = 0
+
+    #Rebuilding main array with multiple smaller arrays of 1 element length
+    #going through index positions in both L AND R array
+    while i < len(L_list) and j < len(R_list):
+        #comparison
+        if L_list[i] <= R_list[j]:
+            main_list[k] = L_list[i]
+            #adding to main array and moving pointer on L_List array over 1
+            i += 1
+        else:
+            main_list[k] = R_list[j]
+            #adding to main array and moving pointer on R_List array over 1
+            j += 1
+        #Moving down the index of merged array, to keep adding elements
+        k += 1
+
+    #continues reading off L_list if R_list finishes first
+    while i < len(L_list):
+        main_list[k] = L_list[i]
+        i += 1
+        k += 1
+
+    #continues reading off R_list if L_list finishes first
+    while j < len(R_list):
+        main_list[k] = R_list[j]
+        j += 1
+        k += 1
+
+original_list = [26,64,11,34,25,90,12]
+print('Input Array:')
+print(original_list)
+
+mergeSort(original_list)
+print('\nSorted Array:')
+print(original_list)
